8ca715cc680bc73623ea8c89eb5e719b040a2c8f
ozoraaaaaa <123875082+ozoraaaaaa@users.noreply.github.com>
2023-01-29 21:33:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+"""
+ini fungsi
+"""
+def luas_segitiga():
+    alas=int(input("masukkan alas : "))
+    tinggi=int(input("masukkan tinggi : "))
+    luas=(alas*tinggi/2)
+    print("luas segitiga adalah: ", luas)
+
+
+luas_segitiga()
